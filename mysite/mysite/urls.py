6f6feffa68463c4de899c9249d94d648e26fd898
Nikita Lyubchich <cybran111@gmail.com>
2017-08-12 19:07:08 +0300

    solr

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from polls.views import index
+from polls.views import index, solr
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name='index'),
+    url(r'^solr$', solr, name='index'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+import requests
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def solr(request):
+    return HttpResponse(
+        requests.get(
+            'http://localhost:8983/solr/techproducts/select?q=inStock:false&wt=json&fl=id,name'
+        ).text
+    )
